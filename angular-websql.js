a2ca14c109ec4513d2d655a648591252dc227eb6
cristoferdomingues <cristoferdomingues@msn.com>
2014-07-29 17:36:05 -0300

    insertOrReplace method added
    db.insertOrReplace('table',persistObj)

--- angular-websql.js
+++ angular-websql.js
@@ -4,154 +4,170 @@
  * © MIT License
  */
 "use strict";
-angular.module("angular-websql", []).factory("$webSql", [
-    function() {
-      return {
-        openDatabase: function(dbName, version, desc, size) {
-          try {
-            var db = openDatabase(dbName, version, desc, size);
-            if (typeof(openDatabase) == "undefined")
-              throw "Browser does not support web sql";
-            return {
-              executeQuery: function(query, values, callback) {
-                console.log(query, values);
-                db.transaction(function(tx) {
-                  tx.executeSql(query, values, function(tx, results) {
-                    if (callback)
-                      callback(results);
-                  });
+angular.module("angular-websql", []).factory("$webSql", ["$q",
+  function($q) {
+    return {
+      openDatabase: function(dbName, version, desc, size) {
+        try {
+          var db = openDatabase(dbName, version, desc, size);
+          if (typeof(openDatabase) == "undefined")
+            throw "Browser does not support web sql";
+          return {
+            executeQuery: function(query, values) {
+              var deferred = $q.defer();
+              console.log(query, values);
+              db.transaction(function(tx) {
+                tx.executeSql(query, values, function(tx, results) {
+                  deferred.resolve(results);
+                }, function(tx, e) {
+                  console.log("There has been an error: " + e.message);
+                  deferred.reject();
                 });
-                return this;
-              },
-              insert: function(c, e, callback) {
-                var f = "INSERT INTO `{tableName}` ({fields}) VALUES({values});";
-                var a = "",
+              });
+
+              return deferred.promise;;
+            },
+            insert: function(c, e) {
+              var f = "INSERT INTO `{tableName}` ({fields}) VALUES({values});";
+              var a = "",
                 b = "",
                 v = [];
-                for (var d in e) {
-                  a += (Object.keys(e)[Object.keys(e).length - 1] == d) ? "`" + d + "`" : "`" + d + "`, ";
-                  b += (Object.keys(e)[Object.keys(e).length - 1] == d) ? "?" : "?, ";
-                  v.push(e[d]);
-                }
-                this.executeQuery(this.replace(f, {
-                  "{tableName}": c,
-                  "{fields}": a,
-                  "{values}": b
-                }), v, callback);
-                return this;
-              },
-              update: function(b, g, c, callback) {
-                var f = "UPDATE `{tableName}` SET {update} WHERE {where}; ";
-                var e = "";
-                var v = [];
-                for (var d in g) {
-                  e += "`" + d + "`= ?";
-                  v.push(g[d]);
-                }
-                var a = this.whereClause(c);
-                this.executeQuery(this.replace(f, {
-                  "{tableName}": b,
-                  "{update}": e,
-                  "{where}": a.w
-                }), v.concat(a.p), callback);
-                return this;
-              },
-              del: function(b, c, callback) {
-                var d = "DELETE FROM `{tableName}` WHERE {where}; ";
-                var a = this.whereClause(c);
-                this.executeQuery(this.replace(d, {
-                  "{tableName}": b,
-                  "{where}": a.w
-                }), a.p, callback);
-                return this;
-              },
-              select: function(b, c, callback) {
-                var d = "SELECT * FROM `{tableName}` WHERE {where}; ";
-                var a = this.whereClause(c);
-                this.executeQuery(this.replace(d, {
-                  "{tableName}": b,
-                  "{where}": a.w
-                }), a.p, callback);
-                return this;
-              },
-              selectAll: function(a, callback) {
-                this.executeQuery("SELECT * FROM `" + a + "`; ", [], callback);
-                return this;
-              },
-              whereClause: function(b, callback) {
-                var a = "",
+              for (var d in e) {
+                a += (Object.keys(e)[Object.keys(e).length - 1] == d) ? "`" + d + "`" : "`" + d + "`, ";
+                b += (Object.keys(e)[Object.keys(e).length - 1] == d) ? "?" : "?, ";
+                v.push(e[d]);
+              }
+              return this.executeQuery(this.replace(f, {
+                "{tableName}": c,
+                "{fields}": a,
+                "{values}": b
+              }), v);
+            },
+            insertOrReplace: function(c, e) {
+              var f = "INSERT OR REPLACE INTO `{tableName}` ({fields}) VALUES({values});";
+              var a = "",
+                b = "",
                 v = [];
-                for (var c in b) {
-                  if(typeof b[c] !== "undefined" && typeof b[c] !== "object" && typeof b[c] === "string" && !b[c].match(/NULL/ig)) v.push(b[c]);
-                  else if(typeof b[c] !== "undefined" && typeof b[c] !== "object" && typeof b[c] === "number") v.push(b[c]);
-                  else if(typeof b[c]["value"] !== "undefined" && typeof b[c] === "object" && !b[c]["value"].match(/NULL/ig)) v.push(b[c]["value"]);
-                  a += (typeof b[c] === "object") ? 
-                          (typeof b[c]["union"] === "undefined") ? 
-                            (typeof b[c]["value"] === "string" && b[c]["value"].match(/NULL/ig)) ? 
-                              "`" + c + "` " + b[c]["value"] : 
-                              "`" + c + "` " + b[c]["operator"] + " ? " : 
-                            (typeof b[c]["value"] === "string" && b[c]["value"].match(/NULL/ig)) ? 
-                              "`" + c + "` " + b[c]["value"] + " " + b[c]["union"] + " " : 
-                              "`" + c + "` " + b[c]["operator"] + " ? " + b[c]["union"] + " " : 
-                          (typeof b[c] === "string" && b[c].match(/NULL/ig)) ? 
-                            "`" + c + "` " + b[c] : 
-                            "`" + c + "`=?"
+              for (var d in e) {
+                a += (Object.keys(e)[Object.keys(e).length - 1] == d) ? "`" + d + "`" : "`" + d + "`, ";
+                b += (Object.keys(e)[Object.keys(e).length - 1] == d) ? "?" : "?, ";
+                v.push(e[d]);
+              }
+              return this.executeQuery(this.replace(f, {
+                "{tableName}": c,
+                "{fields}": a,
+                "{values}": b
+              }), v);
+            },
+            update: function(b, g, c) {
+              var f = "UPDATE `{tableName}` SET {update} WHERE {where}; ";
+              var e = "";
+              var v = [];
+              for (var d in g) {
+                e += "`" + d + "`= ?";
+                v.push(g[d]);
+              }
+              var a = this.whereClause(c);
+              return this.executeQuery(this.replace(f, {
+                "{tableName}": b,
+                "{update}": e,
+                "{where}": a.w
+              }), v.concat(a.p));
+            },
+            del: function(b, c) {
+              var d = "DELETE FROM `{tableName}` WHERE {where}; ";
+              var a = this.whereClause(c);
+              return this.executeQuery(this.replace(d, {
+                "{tableName}": b,
+                "{where}": a.w
+              }), a.p);
+            },
+            select: function(b, c) {
+              var d = "SELECT * FROM `{tableName}` WHERE {where}; ";
+              var a = this.whereClause(c);
+              return this.executeQuery(this.replace(d, {
+                "{tableName}": b,
+                "{where}": a.w
+              }), a.p);
+            },
+            selectAll: function(a) {
+              return this.executeQuery("SELECT * FROM `" + a + "`; ", []);
+            },
+            whereClause: function(b) {
+              var a = "",
+                v = [];
+              for (var c in b) {
+                if (typeof b[c] !== "undefined" && typeof b[c] !== "object" && typeof b[c] === "string" && !b[c].match(/NULL/ig)) v.push(b[c]);
+                else if (typeof b[c] !== "undefined" && typeof b[c] !== "object" && typeof b[c] === "number") v.push(b[c]);
+                else if (typeof b[c]["value"] !== "undefined" && typeof b[c] === "object" && !b[c]["value"].match(/NULL/ig)) v.push(b[c]["value"]);
+                a += (typeof b[c] === "object") ?
+                  (typeof b[c]["union"] === "undefined") ?
+                  (typeof b[c]["value"] === "string" && b[c]["value"].match(/NULL/ig)) ?
+                  "`" + c + "` " + b[c]["value"] :
+                  "`" + c + "` " + b[c]["operator"] + " ? " :
+                  (typeof b[c]["value"] === "string" && b[c]["value"].match(/NULL/ig)) ?
+                  "`" + c + "` " + b[c]["value"] + " " + b[c]["union"] + " " :
+                  "`" + c + "` " + b[c]["operator"] + " ? " + b[c]["union"] + " " :
+                  (typeof b[c] === "string" && b[c].match(/NULL/ig)) ?
+                  "`" + c + "` " + b[c] :
+                  "`" + c + "`=?"
+              }
+              return {
+                w: a,
+                p: v
+              };
+            },
+            replace: function(a, c) {
+              for (var b in c) {
+                a = a.replace(new RegExp(b, "ig"), c[b])
+              }
+              return a;
+            },
+            createTable: function(j, g) {
+              var b = "CREATE TABLE IF NOT EXISTS `{tableName}` ({fields}); ";
+              var c = [];
+              var a = "";
+              for (var e in g) {
+                var l = "{type} {null}";
+                a += "`" + e + "` ";
+                if (typeof g[e]["null"] === "undefined") g[e]["null"] = "NULL";
+                for (var k in g[e]) {
+                  l = l.replace(new RegExp("{" + k + "}", "ig"), g[e][k])
                 }
-                return {w:a,p:v};
-              },
-              replace: function(a, c, callback) {
-                for (var b in c) {
-                  a = a.replace(new RegExp(b, "ig"), c[b])
+                a += l;
+                if (typeof g[e]["default"] !== "undefined") {
+                  a += " DEFAULT " + g[e]["default"]
                 }
-                return a;
-              },
-              createTable: function(j, g, callback) {
-                var b = "CREATE TABLE IF NOT EXISTS `{tableName}` ({fields}); ";
-                var c = [];
-                var a = "";
-                for (var e in g) {
-                  var l = "{type} {null}";
-                  a += "`" + e + "` ";
-                  if(typeof g[e]["null"]==="undefined") g[e]["null"]="NULL";
-                  for (var k in g[e]) {
-                    l = l.replace(new RegExp("{" + k + "}", "ig"), g[e][k])
-                  }
-                  a += l;
-                  if (typeof g[e]["default"] !== "undefined") {
-                    a += " DEFAULT " + g[e]["default"]
-                  }
-                  if (typeof g[e]["primary"] !== "undefined") {
-                    a += " PRIMARY KEY"
-                  }
-                  if (typeof g[e]["auto_increment"] !== "undefined") {
-                    a += " AUTOINCREMENT"
-                  }
-                  if (Object.keys(g)[Object.keys(g).length - 1] != e) {
-                    a += ","
-                  }
-                  if (typeof g[e]["primary"] !== "undefined" && g[e]["primary"]) {
-                    c.push(e)
-                  }
+                if (typeof g[e]["primary"] !== "undefined") {
+                  a += " PRIMARY KEY"
                 }
-                var d = {
-                  tableName: j,
-                  fields: a
-                };
-                for (var f in d) {
-                  b = b.replace(new RegExp("{" + f + "}", "ig"), d[f])
+                if (typeof g[e]["auto_increment"] !== "undefined") {
+                  a += " AUTOINCREMENT"
                 }
-                this.executeQuery(b, [], callback);
-                return this;
-              },
-              dropTable: function(a, callback) {
-                this.executeQuery("DROP TABLE IF EXISTS `" + a + "`; ", [], callback);
-                return this;
-              },
-            };
-          } catch (err) {
-            console.error(err);
-          }
+                if (Object.keys(g)[Object.keys(g).length - 1] != e) {
+                  a += ","
+                }
+                if (typeof g[e]["primary"] !== "undefined" && g[e]["primary"]) {
+                  c.push(e)
+                }
+              }
+              var d = {
+                tableName: j,
+                fields: a
+              };
+              for (var f in d) {
+                b = b.replace(new RegExp("{" + f + "}", "ig"), d[f])
+              }
+              return this.executeQuery(b, []);
+            },
+            dropTable: function(a) {
+              return this.executeQuery("DROP TABLE IF EXISTS `" + a + "`; ", []);
+            },
+          };
+        } catch (err) {
+          console.error(err);
         }
       }
     }
+  }
 ]);
